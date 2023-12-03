ad07946b353354f0a9e85b432352c3fc08483da0
jessie-sr <jessie_sr@163.com>
2023-12-03 00:29:22 -0800

    Added user profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable, :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :translations, dependent: :destroy
+  has_one_attached :profile_image
 
   before_save :set_username
 
