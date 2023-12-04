473b87faf696aac89d31e65eb1778256c7835d1d
jessie-sr <jessie_sr@163.com>
2023-12-04 00:15:55 -0800

    Verified assets precompilation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,5 +11,6 @@ Rails.application.config.assets.version = "1.0"
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w( custom.js )
+Rails.application.config.assets.precompile += %w( custom.js custom.css )
+
 
