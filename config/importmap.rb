7a77cf7258476dcc9cf1175c3572f03405258ca1
jessie-sr <jessie_sr@163.com>
2023-12-04 21:07:31 -0800

    Attempted improving confirmation dialog

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.1/app/assets/javascripts/rails-ujs.esm.js"
+pin "custom", to: "custom.js"
