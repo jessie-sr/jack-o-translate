23bd9d0cf8417c9d5a54c4e31b7f4a9da816b7aa
jessie-sr <jessie_sr@163.com>
2023-12-04 20:23:59 -0800

    Updated application_controller and enabled profile image upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
     protected
-
+  
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :profile_image])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username, :profile_image])
     end
-end
+  end
+  
