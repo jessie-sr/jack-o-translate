ed66d4c739f296ef9b651619c63c0b6f8b9bcfa7
jessie-sr <jessie_sr@163.com>
2023-12-04 21:56:19 -0800

    Updated create method for responses_controller

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,3 +1,20 @@
 class ResponsesController < ApplicationController
     before_action :authenticate_user!
+
+    def create
+        @post = Post.find(params[:post_id])
+        @response = @post.responses.create(response_params.merge(user: current_user))
+        if @response.save
+          redirect_to @post, notice: 'Response was successfully added.'
+        else
+          redirect_to @post, alert: 'Unable to add response.'
+        end
+    end
+      
+    private
+      
+    def response_params
+        params.require(:response).permit(:content)
+    end
+      
 end
